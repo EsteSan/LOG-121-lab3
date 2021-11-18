1a5a17f907b7e06bd6b41bc79d006e5447ab65ea
Jurrwensky <jurrwensky12@live.com>
2021-11-18 13:46:57 -0500

    Test

---
+++ Labo3/src/Image.java
@@ -0,0 +1,16 @@
+public class Image {
+
+    String pathImage;
+
+    public Image(String pathImage){
+        this.pathImage=pathImage;
+    }
+
+    public String getPathImage() {
+        return pathImage;
+    }
+
+    public void setPathImage(String pathImage) {
+        this.pathImage = pathImage;
+    }
+}
---
+++ Labo3/src/Perspective.java
@@ -0,0 +1,11 @@
+public class Perspective extends Image {
+
+    public Perspective(String pathImage) {
+        super(pathImage);
+    }
+
+    int mulitplicateurZoom;
+
+
+
+}
